5ec9096d7f16f835b380ca66be20eb16cbcee5b0
sovahouse <sovahouse7@gmail.com>
2016-01-26 01:24:33 +0200

    HomeTask4

---
+++ module4/src/Converter.java
@@ -0,0 +1,16 @@
+
+public class Converter {
+
+    public static double convertToCelsius (double f) {
+
+        return (f - 32) / 1.8;
+
+    }
+
+    public static double convertToFahrenheit (double c) {
+
+        return (c * 1.8) + 32;
+
+    }
+
+}
