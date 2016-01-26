e67525a2453b682e8a2c2da84ba8d74f3c04ee7e
sovahouse <sovahouse7@gmail.com>
2016-01-26 23:36:18 +0200

    update

---
+++ module4/Length.java
@@ -0,0 +1,11 @@
+
+public class Length {
+
+    public static double calculate(double x1, double y1,
+                                   double x2, double y2) {
+
+        return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+
+    }
+
+}
