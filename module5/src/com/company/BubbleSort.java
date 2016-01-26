ed6c325eea0651b8673e22ad538768d530eac21b
sovahouse <sovahouse7@gmail.com>
2016-01-26 23:14:46 +0200

    Module5

---
+++ module5/src/com/company/BubbleSort.java
@@ -0,0 +1,28 @@
+package com.company;
+
+
+public class BubbleSort {
+
+    public static int[] sort(int[] arr) {
+
+        int tmp;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            for (int j = 0; j < arr.length - i - 1; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+
+                    tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+
+                }
+            }
+        }
+
+        return arr;
+
+    }
+
+}
