671975ac3a07eec3b30b941b318bdc53d453e30c
sovahouse <sovahouse7@gmail.com>
2016-02-04 23:09:38 +0200

    update

--- module9/task1/CesarCipher.java
+++ module9/task1/CesarCipher.java
@@ -25,33 +25,18 @@ public class CesarCipher {
 
     public String encode(String baseString) {
 
-        int sizeOfAlphabet = alphabet.size();
-        char ch;
-        int index;
-        StringBuilder encodedString = new StringBuilder();
-
-        for (int i = 0; i < baseString.length(); i++) {
-
-            ch = baseString.charAt(i);
-            index = alphabet.indexOf(ch);
-
-            if(index == -1) {                                                                                           //if symbol not found in alphabet don't change it.
-
-                encodedString.append(ch);
-                continue;
-            }
-
-            index = (index + 11) % sizeOfAlphabet;
-            encodedString.append(alphabet.get(index));
-
-        }
-
-        return encodedString.toString();
+        return enDecode(baseString, true);
 
     }
 
     public String decode(String baseString) {
 
+        return enDecode(baseString, false);
+
+    }
+
+    private String enDecode(String baseString, boolean b) {
+
         int sizeOfAlphabet = alphabet.size();
         char ch;
         int index;
@@ -62,13 +47,21 @@ public class CesarCipher {
             ch = baseString.charAt(i);
             index = alphabet.indexOf(ch);
 
-            if(index == -1) {                                                                                           //if symbol not found in alphabet don't change it.
+            if (index == -1) {                                                                                           //if symbol not found in alphabet don't change it.
 
                 encodedString.append(ch);
                 continue;
+
             }
 
-            index = (index - 11) % sizeOfAlphabet;
+            if (b == true) {
+
+                index = (index + 11) % sizeOfAlphabet;
+
+            } else {
+                index = (index - 11) % sizeOfAlphabet;
+            }
+
             encodedString.append(alphabet.get(index));
 
         }
@@ -77,6 +70,8 @@ public class CesarCipher {
 
     }
 
-
-
 }
+
+
+
+
