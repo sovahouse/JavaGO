c37371c24eec2b9b5f43054ba533a90e8ec8972a
sovahouse <sovahouse7@gmail.com>
2016-03-03 13:40:00 +0200

    stabilityPeriod

--- codegym/3/src/Runner.java
+++ codegym/3/src/Runner.java
@@ -4,7 +4,7 @@ public class Runner {
     public static void main(String[] args) {
 
         LongestStabilityPeriod longestStabilityPeriod = new LongestStabilityPeriod();
-        int x[] = {900};
+        int x[] = {902, 902, 901, 902, 902, 903, 902, 902, 901, 902, 901, 901, 902, 903};
         System.out.println(longestStabilityPeriod.count(x));
 
 
