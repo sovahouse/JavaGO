e43dd42a8fdf7e849fba41b278804687a270d64e
sovahouse <sovahouse7@gmail.com>
2016-03-12 23:53:04 +0200

    marge

--- codegym/3/src/Runner.java
+++ codegym/3/src/Runner.java
@@ -3,10 +3,6 @@ public class Runner {
 
     public static void main(String[] args) {
 
-        /*String s = "one,two\nthree,";
-
-        CSVParser csvParser = new CSVParser();
-        System.out.println(csvParser.parse(s));*/
 
 
     }
