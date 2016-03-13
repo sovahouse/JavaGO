7b95bfa28d849f4c8e6db9bb5b9067e6507cc325
sovahouse <sovahouse7@gmail.com>
2016-03-13 16:12:27 +0200

    HW1

--- Enterprise/HW1/src/CollectionEfficiency.java
+++ Enterprise/HW1/src/CollectionEfficiency.java
@@ -1,7 +1,27 @@
+import java.time.Clock;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CollectionEfficiency {
 
     public static void main(String[] args) {
 
-    }
+        List<Integer> arrayList = new ArrayList<>();
 
+
+        Clock clock = Clock.systemUTC();
+        long start = clock.millis();
+
+        for(int i = 0; i != 1000000; i++) {
+            arrayList.add(i);
+        }
+        for(int i = 0; i != 1000000; i++) {
+            arrayList.get(i);
+        }
+
+
+        System.out.println(clock.millis() - start);
+
+    }
 }
+
