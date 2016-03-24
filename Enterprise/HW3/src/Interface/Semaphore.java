b15f31b6e8612d0b59f290a0f012ab68b7b21db5
sovahouse <sovahouse7@gmail.com>
2016-03-25 01:10:15 +0200

    Homework 3 done

--- Enterprise/HW3/src/Interface/Semaphore.java
+++ Enterprise/HW3/src/Interface/Semaphore.java
@@ -4,24 +4,24 @@ public interface Semaphore {
 
     // Запрашивает разрешение. Если есть свободное захватывает его. Если нет - приостанавливает поток до тех пор пока не появится свободное разрешение.
 
-    void acquire() throws InterruptedException;
+    public void acquire() throws InterruptedException;
 
     // Запрашивает переданое количество разрешений. Если есть переданое количество свободных разрешений захватывает их.
 
     // Если нет - приостанавливает поток до тех пор пока не появится переданое колтчество свободных разрешений.
 
-    void acquire(int permits) throws InterruptedException;
+    public void acquire(int permits) throws InterruptedException;
 
     // Отпускает разрешение возвращая его семафору.
 
-    void release();
+    public void release();
 
     // Отпускает переданое количество разрешений возварщая их семафору.
 
-    void release(int permits);
+    public void release(int permits);
 
     // Возвращает количество свободных разрешений на данный момент.
 
-    int getAvailablePermits();
+    public int getAvailablePermits();
 
 }
