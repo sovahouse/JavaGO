f24dbaf82108fc8abd2b34e8c63028df45474b50
sovahouse <sovahouse7@gmail.com>
2016-07-07 18:24:34 +0300

    HW6.1 add find by name function in Employee

--- Enterprise/HW6.2/src/test/java/goit/hw6/model/jdbc/JdbcEmployeeDaoTest.java
+++ Enterprise/HW6.2/src/test/java/goit/hw6/model/jdbc/JdbcEmployeeDaoTest.java
@@ -1,10 +1,19 @@
 package goit.hw6.model.jdbc;
 
+import goit.hw6.Main;
+import goit.hw6.model.Employee;
+import goit.hw6.model.EmployeeDao;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class JdbcEmployeeDaoTest {
+
+    private static JdbcEmployeeDao jdbcEmployeeDao = new JdbcEmployeeDao();
+
     @Test
     public void addEmployee() throws Exception {
 
@@ -17,7 +26,10 @@ public class JdbcEmployeeDaoTest {
 
     @Test
     public void findByName() throws Exception {
-
+        String name = "Robert";
+        List<Employee> result = jdbcEmployeeDao.findByName(name);
+        System.out.printf("");
+        //assertEquals(name, result.getFirstName());
     }
 
 }
