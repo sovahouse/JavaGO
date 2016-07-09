ed4d03bd7e8350b391feaa5be888957c9a2cdf6c
sovahouse <sovahouse7@gmail.com>
2016-07-09 17:43:20 +0300

    HW6.1 add menu class

---
+++ Enterprise/HW6.2/src/main/java/goit/hw6/model/Menu.java
@@ -0,0 +1,25 @@
+package goit.hw6.model;
+
+import java.util.List;
+
+public class Menu {
+
+    private String name;
+    private List<Dish> dishes;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Dish> getDishes() {
+        return dishes;
+    }
+
+    public void setDishes(List<Dish> dishes) {
+        this.dishes = dishes;
+    }
+}
