019b2639c24d84090b036f52924c4351a720d18f
sovahouse <sovahouse7@gmail.com>
2016-07-12 17:19:10 +0300

    HW8.1 start

---
+++ Enterprise/HW8.1/src/main/java/goit/hw8_1/servlets/TODOServlet.java
@@ -0,0 +1,17 @@
+package goit.hw8_1.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class TODOServlet extends HttpServlet{
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println("Ziga!");
+    }
+}
